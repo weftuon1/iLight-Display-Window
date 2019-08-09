96ad309265dd05219c738b18b7758c4c05ccb544
liuy3 <50950539+liuy3@users.noreply.github.com>
2019-08-09 22:19:17 +0800

    Add files via upload

--- src/pwm/servo/Servo.c
+++ src/pwm/servo/Servo.c
@@ -1,17 +1,94 @@
 #include "Servo.h"
 
+void attach(uint32_t pin, uint32_t ch){
+    int32_t statePWM = 0;
+    _pwm_cfg->mode=DEV_PWM_TIMER_MODE_PWM;
+    
+    io_arduino_config(pin, ARDUINO_PWM, IO_PINMUX_ENABLE);
+    _pwm = pwm_timer_get_dev(IOTDK_PWM_ID);
+    _ch=ch;
+    _pin=pin;
+    _attached=true;
 
-Servo::Servo(){
+    statePWM=_pwm->pwm_timer_open();
+    if (statePWM == E_OPNED)
+	{
+		_pwm->pwm_timer_control(ch, PWM_TIMER_CMD_ENA_CH, NULL);
+
+	}else if(statePWM == E_OK){
+        printf("Success\n");
+    }
+
+    /* statePWM=_pwm ->pwm_timer_write(ch, DEV_PWM_TIMER_MODE_PWM, REFRESH_FREQUENCY, DEFAULT_PULSE_WIDTH);//90 degree
+    if(statePWM == E_OK){
+        printf("Success \n");
+    }else if(statePWM == E_OBJ){
+        printf("E_OBJ\n");
+    }else if(statePWM == E_PAR){
+        printf("E_PAR\n");
+    }else if(statePWM == E_SYS){
+        printf("E_SYS\n");
+    }*/
+    board_delay_ms(10,1);
+
+    /* uint32_t m;
+    uint32_t f;
+    uint32_t d;
+
+    statePWM= _pwm->pwm_timer_read(ch, &(_pwm_cfg->mode), &f, &d);
+    if(statePWM == E_OK){
+        printf("Success\n");
+    }else if(statePWM == E_OBJ){
+        printf("E_OBJ\n");
+    }else if(statePWM == E_PAR){
+        printf("E_PAR\n");
+    }else if(statePWM == E_SYS){
+        printf("E_SYS\n");
+    }else{
+        printf("WTF\n");
+    }*/
+
+}
+void write(double angle){
+    if(!_attached){
+        printf("Not attach yet");
+    }else if(angle<0||angle>180){
+        printf("angle is out of range");
+    }else{
+
+   
+        _angle=angle;
+        double rang = 180.0 / (MAX_PULSE_WIDTH - MIN_PULSE_WIDTH);
+        double dd=(angle) / rang + MIN_PULSE_WIDTH;
+        uint32_t duty = dd;
+ 
+       int32_t statePWM = 0;
+        statePWM=_pwm ->pwm_timer_write(_ch, DEV_PWM_TIMER_MODE_PWM, REFRESH_FREQUENCY, duty);
+        //sys_msleep(10);
+        board_delay_ms(100,1);
+        if(statePWM == E_OK){
+            printf("Success %f, %d\n", dd, duty);
+        }
+    }
+}
+void detach(){
+    if(!_attached){
+        printf("Not attach yet");
+    }else{
+        _attached=false;
+        _pwm->pwm_timer_close();
+    }
+}
+/* Servo::Servo(){
     _angle = 0.0;
-    _value = 0;
+    //_value = 0;
     _attached = False;
     _pwm_cfg->mode=DEV_PWM_TIMER_MODE_PWM;
     _pwm = pwm_timer_get_dev(IOTDK_PWM_ID);
-}
+}*/
 
-void Servo::attach(uint32_t pin, uint32_t ch){
+/* void Servo::attach(uint32_t pin, uint32_t ch){
     io_arduino_config(pin, ARDUINO_PWM, IO_PINMUX_ENABLE);
-
     _ch=ch;
     _pin=pin;
     _attached=True;
@@ -21,9 +98,9 @@ void Servo::attach(uint32_t pin, uint32_t ch){
 	}
     _pwm ->pwm_timer_write(ch, _pwm_cfg->mode, REFRESH_FREQUENCY, DEFAULT_PULSE_WIDTH);
     //sys_msleep(10);//t ms
-}
+}*/
 
-void Servo::write(double angle){
+/* void Servo::write(double angle){
     if(!attached()){
         printf("Not attach yet");
     }else if(angle<0||angle>180){
@@ -32,12 +109,12 @@ void Servo::write(double angle){
         _angle=angle;
         double rang = 180.0 / (MAX_PULSE_WIDTH - MIN_PULSE_WIDTH);
         double duty = (angle) / rang + MIN_PULSE_WIDTH;
-        _pwm ->pwm_timer_write(ch, _pwm_cfg->mode, REFRESH_FREQUENCY, duty);
+        _pwm ->pwm_timer_write(_ch, _pwm_cfg->mode, REFRESH_FREQUENCY, duty);
     }
     
-}
+}*/
 
-bool Servo::attached(){
+/* bool Servo::attached(){
     return _attached;
 }
 
@@ -46,6 +123,7 @@ void Servo::detach(){
         printf("Not attach yet");
     }else{
         _attached=False;
-        pwm->pwm_timer_close();
+        _pwm->pwm_timer_close();
     }
 }
+*/
--- src/pwm/servo/Servo.h
+++ src/pwm/servo/Servo.h
@@ -5,20 +5,45 @@
 #include "embARC_debug.h"
 #include <stdio.h>
 
+//#include "sys.h"
+
 // If you double the PWM freqnency, you also have to double the three
     // pulse width parameters
-#define   REFRESH_FREQUENCY    50
-#define   MIN_PULSE_WIDTH      2.72     //   0 degrees: 0.544 ms
-#define   DEFAULT_PULSE_WIDTH  7.50     //  90 degrees: 1.500 ms
-#define   MAX_PULSE_WIDTH      12.00     // 180 degrees: 2.400 ms
+#define   REFRESH_FREQUENCY    400//50*5
+#define   MIN_PULSE_WIDTH      22//2.72*5    //   0 degrees: 0.544 ms
+#define   DEFAULT_PULSE_WIDTH  60//7.50*5     //  90 degrees: 1.500 ms
+#define   MAX_PULSE_WIDTH      96//12.00*5     // 180 degrees: 2.400 ms
 
 #define IOTDK_PWM_ID		        DW_PWM_TIMER_0_ID
 
+double _angle;
+bool _attached;
+uint32_t _pin;
+uint32_t _ch;
+DEV_PWM_TIMER_PTR       _pwm;
+DEV_PWM_TIMER_CFG_PTR   _pwm_cfg;
 
-Class Servo{
+
+/* typedef struct servo{
+    double angle;
+
+    bool attached;
+    uint32_t pin;
+    uint32_t ch;
+    DEV_PWM_TIMER_PTR       pwm;
+    DEV_PWM_TIMER_CFG_PTR   pwm_cfg;
+
+}SERVO, *SERVO_PTR;*/
+
+
+void attach(uint32_t pin, uint32_t ch);
+void write(double angle);
+void detach();
+
+/* Class Servo{
     private:
         double _angle;
-        int _val;
+        //int _val;
         bool _attached;
         uint32_t _pin;
         uint32_t _ch;
@@ -31,6 +56,6 @@ Class Servo{
         void write(double angle);
         bool attached();
         void detach();
-};
+};*/
 
 #endif
